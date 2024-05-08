e16bbdca38e332a179ac2d296411fe919333bf93
Emil Wennström <emilwennstrom@gmail.com>
2024-05-08 12:46:50 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,10 +1,10 @@
 from . import *
 
 
-def predict_single_model(df, model):
+def predict_binary_single_model(df, model, prompt_str):
     try:
         for index, row in tqdm(df.iterrows(), total=df.shape[0], desc=f"Processing... "):
-            prompt = ChatPromptTemplate.from_messages([("system", usable_review_binary_prompt), ("human", f"Review: {row['text']}")])
+            prompt = ChatPromptTemplate.from_messages([("system", prompt_str), ("human", f"Review: {row['text']}")])
             answer = query_model(model=model, prompt=prompt)
             if 'True' in answer:
                 df.at[index, 'prediction'] = True
@@ -15,12 +15,12 @@ def predict_single_model(df, model):
     except Exception as e:
         print(f"An error occurred: {e}")
         
-def predict_multiple_models(df, models):
+def predict_binary_multiple_models(df, models, prompt_str):
     try:
         for model_name, model in models:
             print(f"Calling: {model_name}")
             for index, row in tqdm(df.iterrows(), total=df.shape[0], desc=f"Processing {model_name}", leave=False):
-                prompt = ChatPromptTemplate.from_messages([("system", usable_review_binary_prompt), ("human", f"Review: {row['text']}")])
+                prompt = ChatPromptTemplate.from_messages([("system", prompt_str), ("human", f"Review: {row['text']}")])
                 answer = query_model(model=model, prompt=prompt)
                 if 'True' in answer:
                     df.at[index, model_name + "_prediction"] = True
@@ -35,19 +35,14 @@ def predict_multiple_models(df, models):
 
 def main():
     
-    '''
-        Load CSV-Files from folder
-    '''
+    # Load CSV-Files from folder
     df = load_and_convert_all_from_folder()
     
     
-    '''
-
-    '''
     
     
     # Single model
-    predict_single_model(df, chat_bison())
+    predict_binary_single_model(df=df, model=chat_bison(), prompt_str=usable_review_binary_prompt)
     
     print(df.head())
 
